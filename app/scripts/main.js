36781eb4d0d6f7b0a082770d97f19288c75ae123
Kina4smile <kina.girl@gmail.com>
2016-02-03 23:35:09 +0200

    Bug fixed, responsive header fixed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
   $('#menuToggler').click(function() {
     $('#borderOff').css('padding','0')
-  })
+  });
+  maintainSameHeight($('[data-same-height="blockquote-text"]'));
+
+  function maintainSameHeight($list) {
+    var height = 0;
+
+    $list.each(function () {
+      var $this = $(this);
+      if ($this.outerHeight() > height) {
+        height = $this.outerHeight();
+      }
+    });
+
+    $list.css("height", height);
+  }
 });
